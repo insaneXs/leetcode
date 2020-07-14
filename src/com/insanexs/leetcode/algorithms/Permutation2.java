8d8796c742781b9280b9b9489065f64bcaa1e2f3
insaneXs <191717154@qq.com>
2020-07-14 16:58:04 +0800

    Accept Permutation II #47 (Medium)

--- src/com/insanexs/leetcode/algorithms/Permutation2.java
+++ src/com/insanexs/leetcode/algorithms/Permutation2.java
@@ -4,28 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Permutations II #47 (Medium)
+ */
 public class Permutation2 {
-	Integer pre = null;
-	int index = -1;
-	public List<List<Integer>> permuteUnique(int[] num){
+	public List<List<Integer>> permuteUnique(int[] nums){
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
-		if(num == null || num.length < 1)
+		if(nums == null || nums.length < 1)
 			return res;
-		Arrays.sort(num);
+		Arrays.sort(nums);
 		List<Integer> list = new ArrayList<Integer>();
 		List<Integer> index = new ArrayList<Integer>();
-		helper(num,list,res,index);
+		helper(nums, list, res, index);
 		return res;
 	}
-	
+
 	public void helper(int[] num,List<Integer> list,List<List<Integer>> res,List<Integer> index){
 		if(list.size() == num.length){
 			res.add(new ArrayList<Integer>(list));
 			return;
 		}
+		int lastNum = num[0] - 1;
 		for(int i=0; i<num.length; i++){
-			if(index.contains(i))
+			if(index.contains(i) || lastNum == num[i])
 				continue;
+			lastNum = num[i];
 			list.add(num[i]);
 			index.add(i);
 			helper(num, list, res, index);
@@ -35,7 +38,7 @@ public class Permutation2 {
 	}
 	
 	public static void main(String[] args){
-		int[] num = {1,1,2};
+		int[] num = {1,1,2,3};
 		System.out.println(new Permutation2().permuteUnique(num));
 	}
 }
